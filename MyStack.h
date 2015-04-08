f5510c07b24bc4d441e66575a0f33d3928b0569f
James Brosnahan <jb172@zips.uakron.edu>
2015-04-08 19:13:33 -0400

    finished writing function definitions of MyStack.h

--- MyStack.h
+++ MyStack.h
@@ -8,7 +8,7 @@ class MyStack
 private:
    T *stackArray;  // Pointer to the stack array
    int stackSize;    // The max stack size
-   int numElememts;         // Indicates the number of objects on the stack
+   int numElements;         // Indicates the number of objects on the stack
 
 public:
 //// You have to define all the member functions!!!
@@ -17,23 +17,81 @@ public:
 // Constructor                                  *
 // This constructor creates an empty stack.     *
 //***********************************************
-   MyStack(int){;}
+   MyStack(int);
 
    // Copy constructor
-   MyStack(const MyStack &){;}
+   MyStack(const MyStack &);
 
    // Destructor
-   ~MyStack(){;}
+   ~MyStack();
    
    // Stack operations
-   void push(T){;}
-   void pop(){;}
-	T top() {;}
-   bool isFull() const{;}
-   bool isEmpty() const{;}
-	int size() const{;}
+   void push(T);
+   void pop();
+   T top();
+   bool isFull() const;
+   bool isEmpty() const;
+   int size() const;
 }; 
 
 // your code for member functions
+template <class T>
+MyStack<T>::MyStack(int size){
+
+	if(size >= 0){
+		T array[size];
+		stackArray = array;
+		stackSize = size;
+		numElements = 0;}
+	else{
+		
+	}
+
+}
+// Copy constructor
+template <class T>
+MyStack<T>::MyStack(const MyStack &other){
+
+	stackSize = other.stackSize;
+	numElements = other.numElements;
+	T array[stackSize];
+	for(int i = 0; i < numElements; i++) {
+		stackArray[i] = other.stackArray[i];
+	}
+
+}
+// Destructor
+template <class T>
+MyStack<T>::~MyStack(){;}
+// Stack operations
+template <class T>
+void MyStack<T>::push(T obj){
+
+	if(!isFull()){
+		stackArray[numElements++] = obj;
+	}
+	else{
+		
+	}
+
+
+}
+template <class T>
+void MyStack<T>::pop(){
+	if(!isEmpty()){
+		numElements--;
+	}
+	else{
+		
+	}
+}
+template <class T>
+T MyStack<T>::top() {return stackArray[numElements - 1];}
+template <class T>
+bool MyStack<T>::isFull() const{return stackSize == numElements;}
+template <class T>
+bool MyStack<T>::isEmpty() const{return numElements == 0;}
+template <class T>
+int MyStack<T>::size() const{return stackSize;}
 
 #endif 
